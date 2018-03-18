856ed07a47f21bcc5c4162d57b421c8a5b01ddeb
Jennifer Baird <jazbaird@gmail.com>
2018-03-18 17:55:32 -0500

    update

--- scripts/wu-weather.js
+++ scripts/wu-weather.js
@@ -12,3 +12,31 @@ weatherObj.onload = function () {
     document.getElementById('windchill').innerHTML = weatherInfo.current_observation.windchill_string;
 }
 
+function weatherInfo(jsonObj) {
+      var conditions = jsonObj['conditions'];
+      var forecast = jsonObj['forecast'];
+      for(var i = 0; i < weather.length; i++) {
+        var myArticle = document.createElement('article');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+        var myImg = document.createElement('img');  
+        var myPara4 = document.createElement('p');
+        myPara1.textContent = 'Current Weather: ' + current_observation.weather;
+        myPara2.textContent = 'Current Temperature: ' + condition.temp_f;
+        myPara3.textContent = 'Current Wind Speed:' + conditions.wind_mph;
+        myImg.imageRendering = weatherIcon[i].img;
+          
+        var currentWeather = weather[i].current;
+        for(var j = 0; j < scurrentWeather.length; j++) {
+          var listItem = document.createElement('li');
+          listItem.textContent = currentWeather[j];
+          myList.appendChild(listItem);
+        }
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myList);
+        section.appendChild(myArticle);
+      }
